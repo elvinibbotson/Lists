64ab5b2f50aec23bcb6c6dee707139a4e122fd67
elvinibbotson <elvinibbotson@outlook.com>
2022-11-04 13:40:21 +0000

    Add files via upload

--- lisztApp.js
+++ lisztApp.js
@@ -4,7 +4,9 @@ function id(el) {
 'use strict';
 // GLOBAL VARIABLES	
 var db=null;
-var items=[];
+// var items=[];
+var lists=[]; // array of list items
+var notes=[]; // array of note items
 var item=null;
 var itemIndex=0;
 var list={};
@@ -41,7 +43,7 @@ id('main').addEventListener('touchend', function(event) {
         depth--;
         if(depth<1) list.id=list.owner=null;
         console.log('list.id: '+list.id+' path: '+path+' depth: '+depth);
-        loadListItems();
+        loadList();
     }
     else if((drag.x>50)&&(depth>0)&&list.type%4==3) {  // drag left to change checklist to 'shopping' view
         console.log("switch to 'check' view");
@@ -69,10 +71,10 @@ id('heading').addEventListener('click',function() {
 	if(depth>0) { // list heading - show item edit dialog
 		id('listTitle').innerHTML='list';
 		id(listField.value=list.name);
-		console.log('edit list header - '+items.length+' items');
+		console.log('edit list header - '+(lists.length+notes.length)+' items');
 		id('checkAlpha').checked=list.type&2;
 		id('checkBoxes').checked=list.type&4;
-		if(items.length>0) { // can only delete empty lists
+		if((lists.length>0)||(notes.length>0)) { // can only delete empty lists
 			id('deleteListButton').style.display='none';
 			console.log('disable delete');
 		}
@@ -84,7 +86,7 @@ id('heading').addEventListener('click',function() {
 	else showDialog('dataDialog',true);
 });
 
-// ADD NEW ITEMS
+// ADD NEW ITEM
 id('buttonNew').addEventListener('click', function(){
     if(depth<2 && list.type>0) { // list above depth 2 - can add sub-list
         showDialog('addDialog',true);
@@ -121,7 +123,7 @@ id('addNoteButton').addEventListener('click',function() {
 	id('deleteNoteButton').style.display='none';
 	item={};
 	item.owner=list.id;
-	item.type=list.type-1;
+	item.type=0;
 	showDialog('noteDialog',true);
 
 })
@@ -134,55 +136,39 @@ id('listUpButton').addEventListener('click', function() {move(true);})
 id('listDownButton').addEventListener
 id('noteUpButton').addEventListener('click', function() {move(true);})
 id('noteDownButton').addEventListener('click', function() {move(false);})
-function move(up) { // move list/note item up/down
-	console.log('move item type '+item.type);
+function move(up) { // move note up/down
+	// for(var i in notes) console.log('note '+i+': '+notes[i].text+' id: '+notes[i].id);
+	console.log('move note '+item.id+' index: '+item.index+'; up is '+up);
     if(up && itemIndex<1) return; // cannot move up if already first...
-    if(!up && (items.length-itemIndex<2)) return; // ...or down if already last
-    console.log("move "+item.text+(up)?" up":" down");
+    if(!up && (notes.length-itemIndex<2)) return; // ...or down if already last
     if(up) item.index--; // shift this item up...
     else item.index++; // ...or down
     var dbTransaction=db.transaction('items',"readwrite");
     var dbObjectStore=dbTransaction.objectStore('items');
 	console.log("database ready");
-    var getRequest=dbObjectStore.get(item.id);
-	getRequest.onsuccess=function(event) {
-	    var data=event.target.result;
-        if(up) data.index--;
-        else data.index++;
-        var putRequest=dbObjectStore.put(data);
+	var putRequest=dbObjectStore.put(item);
+	putRequest.onsuccess=function(event) {
+		console.log('note '+item.id+" updated - index:"+item.index+' type:'+item.type+' owner:'+item.owner);
+		// now move item above/below
+		if(up) {itemIndex--;}
+		else {itemIndex++;}
+		item=notes[itemIndex];
+		if(up) item.index++;
+		else item.index--;
+		putRequest=dbObjectStore.put(item);
 		putRequest.onsuccess=function(event) {
-			console.log('item '+item.index+" updated");
-		};
-		putRequest.onerror=function(event) {console.log("error updating item "+item.index);};
+			console.log('note '+item.id+" updated - index:"+item.index+' type:'+item.type+' owner:'+item.owner);
+			showDialog('noteDialog',false);
+			loadList();
+		}
+		putRequest.onerror=function(event) {console.log("error updating note "+item.index);}
 	}
-	getRequest.onerror=function(event) {console.log('error getting item')};
-    if(up) {
-    	item=items[itemIndex-1]; // shift previous item down...
-    	item.index++;
-    }
-    else {
-    	item=items[itemIndex+1]; // ...or next item up
-    	item.index--;
-    }
-    getRequest=dbObjectStore.get(item.id);
-	getRequest.onsuccess=function(event) {
-	    var data=event.target.result;
-        if(up) data.index++;
-        else data.index--;
-        putRequest=dbObjectStore.put(data);
-		putRequest.onsuccess=function(event) {
-			console.log('item '+item.index+" updated");
-		};
-		putRequest.onerror=function(event) {console.log("error updating item "+item.index);};
-	}
-	getRequest.onerror=function(event) {console.log('error getting item')};
-    populateList();
-    if(up) itemIndex--;
-    else itemIndex++;
+	putRequest.onerror=function(event) {console.log("error updating note "+item.index);}
 }
 
 // NOTE
 id('confirmNoteButton').addEventListener('click',function() {
+	item={};
 	item.text=id('noteField').value;
 	item.type=0;
 	var dbTransaction=db.transaction('items',"readwrite");
@@ -197,23 +183,27 @@ id('confirmNoteButton').addEventListener('click',function() {
         	var putRequest=dbObjectStore.put(data);
 			putRequest.onsuccess=function(event) {
 				console.log('note '+item.index+" updated");
+				loadList();
 			};
 			putRequest.onerror=function(event) {console.log("error updating note "+item.index);};
 		}
 		getRequest.onerror=function(event) {console.log('error getting list')};
 	}
 	else { // no id so adding new note
+		item.owner=list.id;
+		item.type=0;
 		var addRequest=dbObjectStore.add(item);
 		addRequest.onsuccess=function(event) {
 			item.id=event.target.result;
-			console.log("new note "+item.text+" added - id is "+item.id);
+			console.log("new note:"+item.text+"type:"+item.type+" owner:"+item.owner+" added - id is "+item.id);
+			loadList();
 		};
 		addRequest.onerror=function(event) {console.log("error adding new note");};
 	}
 	showDialog('noteDialog',false);
-	itemIndex=null;
-    currentListItem=null;
-	loadListItems();
+	// itemIndex=null;
+    // currentListItem=null;
+	// loadList();
 })
 id('deleteNoteButton').addEventListener('click',function() {
 	var dbTransaction=db.transaction('items',"readwrite");
@@ -223,7 +213,7 @@ id('deleteNoteButton').addEventListener('click',function() {
 	request.onsuccess=function(event) {
 		console.log('note deleted');
 		showDialog('noteDialog',false);
-		loadListItems();
+		loadList();
 	}
 	request.onerror=function(event) {console.log('error deleting note')};
 })
@@ -265,7 +255,7 @@ id('confirmListButton').addEventListener('click',function() {
 	showDialog('listDialog',false);
 	itemIndex=null;
     currentListItem=null;
-	loadListItems();
+	loadList();
 })
 id('deleteListButton').addEventListener('click',function() {
 	var dbTransaction=db.transaction('items',"readwrite");
@@ -279,153 +269,13 @@ id('deleteListButton').addEventListener('click',function() {
 		path.pop();
 		list.id=list.owner;
 		console.log('back to list '+list.id);
-		loadListItems();
+		loadList();
 	}
 	delRequest.onerror=function(event) {console.log('error deleting list')};
 })
 id('cancelListButton').addEventListener('click',function() {
     showDialog('listDialog',false);
 })
-
-// POPULATE LIST
-function populateList() {
-    var listItem;
-    id("list").innerHTML=""; // clear list
-	console.log("populate list for path "+path+" with "+items.length + " items - depth: "+depth);
-	console.log('list type is '+list.type);
-	if(path.length<1)
-    id('heading').innerHTML=list.name;
-	else {
-	    id('heading').innerHTML=path[0];
-	    var i=1;
-	    while(i<path.length) {
-	        id('heading').innerHTML+='.'+path[i++];
-	    }
-	}
-	// separate into lists and notes
-	var listItems=[];
-	var noteItems=[];
-	for(var i in items) {
-		if(items[i].type>0) listItems.push(items[i]);
-		else noteItems.push(items[i]);
-	}
-	// show lists first, sorted alphabetically
-	listItems.sort(function(a,b){ // always sort list items alphabetically...
-		if(a.text.toUpperCase()<b.text.toUpperCase()) return -1;
-		if(a.text.toUpperCase()>b.text.toUpperCase()) return 1;
-		return 0;
-	});
-	// show notes below lists - sorted alphabetically?
-	if(list.type&4) noteItems.sort(function(a,b){ // sort notes alphabetically...
-		if(a.text.toUpperCase()<b.text.toUpperCase()) return -1;
-		if(a.text.toUpperCase()>b.text.toUpperCase()) return 1;
-		return 0;
-	});
-	else noteItems.sort(function(a,b){return a.index-b.index}); // ...or by .index
-	for(var i in listItems) { // list first...
-		console.log('listItem '+i+': '+listItems[i].text);
-		if((list.type&2)&&(items[i].checked)) continue; // don't show checked items
-		listItem=document.createElement('li');
-		listItem.index=i;
-		listItem.innerText=listItems[i].text;
-		listItem.addEventListener('click',function() {
-	 		itemIndex=this.index;
-	 		item=items[itemIndex];
-	 		console.log('open list '+itemIndex);
-			list.id=listItems[this.index].id;
-			list.type=listItems[this.index].type;
-			list.name=listItems[this.index].text;
-			list.owner=listItems[this.index].owner;
-			console.log('open list '+list.name+' id:'+list.id+' type:'+list.type+' owner: '+list.owner);
-			depth++;
-			path.push(list.name);
-			loadListItems();
-		});
-		listItem.style.fontWeight='bold'; // lists are bold
-		id('list').appendChild(listItem);
-	}
-	for(var i in noteItems) { // ...then notes
-		console.log('noteItem '+i+': '+noteItems[i].text);
-		if((list.type&2)&&(items[i].checked)) continue; // don't show checked items
-		listItem=document.createElement('li');
-		listItem.index=i;
-		if(list.type&2) { // checkbox list
-		    var itemBox=document.createElement('input');
-	 	    itemBox.setAttribute('type','checkbox');
-	 	    itemBox.index=i;
-	 	    itemBox.checked=items[i].checked;
-	 	    itemBox.addEventListener('change',function() {checkItem(this.index);}); // toggle item .checked property
-	 	    listItem.appendChild(itemBox);
-		}
-		var itemText=document.createElement('span');
-	 	itemText.index=i;
-        itemText.innerText=noteItems[i].text;
-	 	listItem.appendChild(itemText);
-	 	listItem.addEventListener('click',function() {
-			itemIndex=this.index;
-			item=noteItems[itemIndex];
-			console.log('note item '+itemIndex+': '+item.text+'; type '+item.type);
-			id('noteTitle').innerHTML='note';
-			id('noteField').innerText=item.text;
-			id('noteUpButton').style.display='block';
-			id('noteDownButton').style.display='block';
-			id('deleteNoteButton').style.display='block';
-			showDialog('noteDialog',true);
-		})
-		console.log('add '+itemText.innerText+' to list');
-		id('list').appendChild(listItem);
-	}
-	
-	/*
-	for(var i in items) {
-	    if((list.type&2)&&(items[i].checked)) continue; // don't show checked items
-		listItem=document.createElement('li');
-		listItem.index=i;
-		if(list.type&2) { // checkbox list
-		    var itemBox=document.createElement('input');
-	 	    itemBox.setAttribute('type','checkbox');
-	 	    itemBox.index=i;
-	 	    itemBox.checked=items[i].checked;
-	 	    itemBox.addEventListener('change',function() {checkItem(this.index);}); // toggle item .checked property
-	 	    listItem.appendChild(itemBox);
-		}
-		var itemText=document.createElement('span');
-	 	itemText.index=i;
-        itemText.innerText=items[i].text;
-	 	listItem.appendChild(itemText);
-	 	if(items[i].type&1) { // tap on a list to open it
-		    listItem.addEventListener('click',function() {
-	 	    	itemIndex=this.index;
-	 	    	item=items[itemIndex];
-	 	    	console.log('open list '+itemIndex);
-		    	list.id=items[this.index].id;
-		    	list.type=items[this.index].type;
-		    	list.name=items[this.index].text;
-		    	list.owner=items[this.index].owner;
-		    	console.log('open list '+list.name+' id:'+list.id+' type:'+list.type+' owner: '+list.owner);
-		    	depth++;
-		    	path.push(list.name);
-		    	loadListItems();
-	 		});
-		    listItem.style.fontWeight='bold'; // lists are bold
-		}
-		else { // tap on note to edit it
-			listItem.addEventListener('click',function() {
-				itemIndex=this.index;
-				item=items[itemIndex];
-				console.log('note item '+itemIndex+': '+item.text+'; type '+item.type);
-				id('noteTitle').innerHTML='note';
-				id('noteField').innerText=item.text;
-				id('noteUpButton').style.display='block';
-				id('noteDownButton').style.display='block';
-				id('deleteNoteButton').style.display='block';
-				showDialog('noteDialog',true);
-			})
-		}
-		id('list').appendChild(listItem);
-	}
-	*/
-}
 
 function checkItem(n) {
     items[n].checked=!items[n].checked;
@@ -448,7 +298,7 @@ function checkItem(n) {
 }
 
 // LOAD LIST ITEMS
-function loadListItems() {
+function loadList() {
 	console.log("load children of list.id "+list.id+" - depth: "+depth+' owner: '+list.owner);
 	var dbTransaction=db.transaction('items',"readwrite");
 	var dbObjectStore=dbTransaction.objectStore('items');
@@ -461,7 +311,6 @@ function loadListItems() {
 			item=event.target.result;
 			console.log("list item "+item.text+"; type: "+item.type+"; owner: "+item.owner);
 			var t=item.text;
-			if(item.secure>0) t=cryptify(t,keyCode);
 			list.name=t;
 			list.type=item.type;
 		};
@@ -471,30 +320,27 @@ function loadListItems() {
 	    list.name="Lists";
 	    list.type=1;
 	}
-	items=[];
+	// items=[];
+	lists=[];
+	notes=[];
 	request=dbObjectStore.openCursor();
 	request.onsuccess=function(event) {
 		var cursor=event.target.result;
 		if(cursor) {
 			if(cursor.value.owner==list.id) { // just items in this list
-				if(cursor.value.type==4) cursor.value.type=0; // **** TEMPORARY FIX ****
-				items.push(cursor.value);
+				// if(cursor.value.type==4) cursor.value.type=0; // **** TEMPORARY FIX ****
+				if(cursor.value.type<1) notes.push(cursor.value); // add to notes[] if type 0...
+				else lists.push(cursor.value); // ...otherwise add to lists[]
+				// items.push(cursor.value);
 				console.log("item id: "+cursor.value.id+"; index: "+cursor.value.index+"; "+cursor.value.text+"; type: "+cursor.value.type+"; owner: "+cursor.value.owner);
 			}
 			cursor.continue ();
 		}
 		else {
-			console.log("No more entries! "+items.length+" items");
+			console.log("No more entries! "+lists.length+" lists; "+notes.length+' notes');
 			if(list.id===null) { // backup checks
-				/* temporary code to remove wrongly numbered data
-				if(items[0].id<1) {
-					alert('CORRUPTED DATA: CLEAR DATABASE');
-					request=dbObjectStore.clear();
-				}
-				*/
-				if(items.length<1) { // no data: restore backup?
+				if((lists.length<1)&&(notes.length<1)) { // no data: restore backup?
 				    console.log("no data - restore backup?");
-				    // document.getElementById('importDialog').style.display='block';
 				    showDialog('importDialog',true);
 				}
 				else { // monthly backups
@@ -505,6 +351,90 @@ function loadListItems() {
 			}
 			populateList();
 		}
+	}
+}
+
+// POPULATE LIST
+function populateList() {
+    var listItem;
+    id("list").innerHTML=""; // clear list
+	console.log("populate list for path "+path+" with "+(lists.length+notes.length)+" items - depth: "+depth);
+	console.log('list type is '+list.type);
+	if(path.length<1)
+    id('heading').innerHTML=list.name;
+	else {
+	    id('heading').innerHTML=path[0];
+	    var i=1;
+	    while(i<path.length) {
+	        id('heading').innerHTML+='.'+path[i++];
+	    }
+	}
+	// show lists first, sorted alphabetically
+	lists.sort(function(a,b){ // always sort list items alphabetically...
+		if(a.text.toUpperCase()<b.text.toUpperCase()) return -1;
+		if(a.text.toUpperCase()>b.text.toUpperCase()) return 1;
+		return 0;
+	});
+	// show notes below lists - sorted alphabetically?
+	if(list.type&4) noteItems.sort(function(a,b){ // sort notes alphabetically...
+		if(a.text.toUpperCase()<b.text.toUpperCase()) return -1;
+		if(a.text.toUpperCase()>b.text.toUpperCase()) return 1;
+		return 0;
+	});
+	else notes.sort(function(a,b){return a.index-b.index}); // ...or by .index
+	for(var i in lists) { // list first...
+		console.log('list '+i+': '+lists[i].text);
+		if((list.type&2)&&(items[i].checked)) continue; // don't show checked items
+		listItem=document.createElement('li');
+		listItem.index=i;
+		listItem.innerText=lists[i].text;
+		listItem.addEventListener('click',function() {
+	 		itemIndex=this.index;
+	 		item=lists[itemIndex];
+	 		console.log('open list '+itemIndex);
+			list.id=lists[this.index].id;
+			list.type=lists[this.index].type;
+			list.name=lists[this.index].text;
+			list.owner=lists[this.index].owner;
+			console.log('open list '+list.name+' id:'+list.id+' type:'+list.type+' owner: '+list.owner);
+			depth++;
+			path.push(list.name);
+			loadList();
+		});
+		listItem.style.fontWeight='bold'; // lists are bold
+		id('list').appendChild(listItem);
+	}
+	for(var i in notes) { // ...then notes
+		console.log('note '+i+': '+notes[i].text);
+		notes[i].index=i;
+		if((list.type&2)&&(items[i].checked)) continue; // don't show checked items
+		listItem=document.createElement('li');
+		listItem.index=i;
+		if(list.type&2) { // checkbox list
+		    var itemBox=document.createElement('input');
+	 	    itemBox.setAttribute('type','checkbox');
+	 	    itemBox.index=i;
+	 	    itemBox.checked=items[i].checked;
+	 	    itemBox.addEventListener('change',function() {checkItem(this.index);}); // toggle item .checked property
+	 	    listItem.appendChild(itemBox);
+		}
+		var itemText=document.createElement('span');
+	 	itemText.index=i;
+        itemText.innerText=notes[i].text;
+	 	listItem.appendChild(itemText);
+	 	listItem.addEventListener('click',function() {
+			itemIndex=this.index;
+			item=notes[itemIndex];
+			console.log('note '+itemIndex+': '+item.text+'; type '+item.type);
+			id('noteTitle').innerHTML='note';
+			id('noteField').innerText=item.text;
+			id('noteUpButton').style.display='block';
+			id('noteDownButton').style.display='block';
+			id('deleteNoteButton').style.display='block';
+			showDialog('noteDialog',true);
+		})
+		console.log('add '+itemText.innerText+' to list');
+		id('list').appendChild(listItem);
 	}
 }
 
@@ -606,7 +536,7 @@ request.onsuccess=function (event) {
 	request.onsuccess=function(event) {
 		
 		list.id=list.owner=null;
-		loadListItems();
+		loadList();
 	};
 };
 request.onupgradeneeded=function(event) {
